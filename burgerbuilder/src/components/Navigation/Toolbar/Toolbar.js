32abda83671d09a950e475295796d1ed91adcf37
krisi01 <chrisisbeast@abv.bg>
2020-12-11 13:45:23 -0800

    Added a navigation bar and styled it.

--- burgerbuilder/src/components/Navigation/Toolbar/Toolbar.js
+++ burgerbuilder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import NavigationItem from '../NavigationItems/NavigationItem/NavigationItem';
 const Toolbar = props => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
         <Logo/>
         <nav>
-            ...
+            <NavigationItems/>
         </nav>
     </header>
 );

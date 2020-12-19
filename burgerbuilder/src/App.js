f754b9a51b135a3dc6dd3a285a38346b686418f0
krisi01 <chrisisbeast@abv.bg>
2020-12-19 12:02:54 -0800

    Adding routing to the burgerbuilder

--- burgerbuilder/src/App.js
+++ burgerbuilder/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import classes from './App.modules.css'
 import Layout from './components/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import {BrowserRouter, Link, Route} from 'react-router-dom';
 function App() {
   return (
     <div className={classes.App}>
+      
       <Layout>
         <BurgerBuilder/>
         </Layout>

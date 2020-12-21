c0b633b1e6d8e80e35d72bb48394820113c69b0a
krisi01 <chrisisbeast@abv.bg>
2020-12-21 06:56:27 -0800

    adding redux

--- burgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -10,12 +10,6 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 import {connect} from 'react-redux';
 
-const INGREDIENT_PRICES = {
-    salad: 0.5,
-    cheese: 0.4,
-    meat: 1.3,
-    bacon: 0.7
-};
 
 class BurgerBuilder extends Component {
     // constructor(props) {
